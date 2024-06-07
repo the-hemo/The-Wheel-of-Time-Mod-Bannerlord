c454df9a4d198b765912c0921e66be105e8165c0
Heragoga <73825102+Heragoga@users.noreply.github.com>
2024-06-07 15:05:30 +0200

    Changed steam update workflow

--- upload_to_steam.bat
+++
@@ -1,3 +0,0 @@
-cd ""C:\Program Files (x86)\Steam\steamapps\common\Mount ^& Blade II Bannerlord\bin\Win64_Shipping_Client""
-TaleWorlds.MountAndBlade.SteamWorkshop.exe C:\Users\Alex\Desktop\WorkshopCreate.xml
-pause
